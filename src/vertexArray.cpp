705e227c8ee49ceb2215f4fbced419e7a08fd39d
Octavio Percivaldi <octaviopercivaldi2@gmail.com>
2022-08-29 19:36:12 +0200

    Added vertexArray class

---
+++ src/vertexArray.cpp
@@ -0,0 +1,36 @@
+#include "vertexArray.hpp"
+
+VertexArray::VertexArray() {
+  glGenVertexArrays(1, &VAO_ID);
+  glBindVertexArray(VAO_ID);
+}
+
+VertexArray::~VertexArray() { glDeleteVertexArrays(1, &VAO_ID); }
+
+void VertexArray::addVertexBufferObject(const VertexBuffer& vbo, const VertexBufferLayout& layout) {
+  // Bind the VertexArray object
+  bind();
+  // Bind the VertexBuffer object
+  vbo.bind();
+
+  const auto elements = layout.getElements();
+  for (int i = 0; i < elements.size(); i++) {
+    const auto& element = elements[i];
+    glEnableVertexAttribArray(i);
+
+    glVertexAttribPointer(
+        i,                   // Attribute index
+        element.count,       // Number of components (e.g. 4 for vec4, in RGBA)
+        element.type,        // Type of the components (e.g. GL_FLOAT)
+        element.normalized,  // Whether to normalize the data
+        layout.getStride(),  // Stride (size of the whole structure, e.g. Position + Colors ~=
+                             // sizeof(Vertex))
+        (const void*)(size_t)
+            element.offset  // Offset of the component in the structure (e.g. offsetOf Color)
+    );
+  }
+}
+
+void VertexArray::bind() { glBindVertexArray(VAO_ID); }
+
+void VertexArray::unbind() { glBindVertexArray(0); }
---
+++ src/vertexArray.hpp
@@ -0,0 +1,34 @@
+#ifndef __VERTEX_ARRAY_HPP__
+#define __VERTEX_ARRAY_HPP__
+
+// Libraries imports
+#include <GLAD/glad.h>
+#include <GLFW/glfw3.h>
+
+// Local imports
+#include "vertex.hpp"
+#include "vertexBuffer.hpp"
+#include "vertexBufferLayout.hpp"
+
+class VertexArray {
+ public:
+  // ID of VertexArray object.
+  unsigned int VAO_ID;
+
+  // Create a new VertexArray object.
+  VertexArray();
+
+  // Delete created VertexArray object.
+  ~VertexArray();
+
+  // Add a VertexBuffer to the VertexArray object.
+  void addVertexBufferObject(const VertexBuffer& vbo, const VertexBufferLayout& layout);
+
+  // Bind created VertexArray object.
+  void bind();
+
+  // Unbind created VertexArray object.
+  void unbind();
+};
+
+#endif  // __VERTEX_ARRAY_HPP__
