23f1244f115387310e477898fdd0990913a1ff44
octavio-percivaldi <octavio.percivaldi-external@taxfix.de>
2022-10-02 14:28:22 -0300

    Extended some classes

--- src/indexBuffer.cpp
+++ src/indexBuffer.cpp
@@ -1,12 +1,19 @@
 #include "indexBuffer.hpp"
 
-IndexBuffer::IndexBuffer(const void* indices, unsigned int size) : size(size) {
+IndexBuffer::IndexBuffer(const void* indices, unsigned int sz) { setup(indices, sz); }
+
+IndexBuffer::~IndexBuffer() { glDeleteBuffers(1, &EBO_ID); }
+
+void IndexBuffer::setup(const void* indices, unsigned int sz) {
+  size = sz;
   glGenBuffers(1, &EBO_ID);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_ID);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, indices, GL_STATIC_DRAW);
 }
 
-IndexBuffer::~IndexBuffer() { glDeleteBuffers(1, &EBO_ID); }
+void IndexBuffer::setup(std::vector<unsigned int>& indices) {
+  setup(&indices[0], indices.size() * sizeof(indices[0]));
+}
 
 void IndexBuffer::bind() const { glBindBuffer(GL_ARRAY_BUFFER, EBO_ID); }
 
